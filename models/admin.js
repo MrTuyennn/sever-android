62a6fb4168825c886d90194e69ad61f5dd472a4c
unknown <nguyenngoctuyen188@gmail.com>
2020-04-09 09:15:46 +0700

    có update

--- models/admin.js
+++ models/admin.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const admin = new Schema({
-    username: { type: String, unique: true, required: true, trim: true },
-    email: { type: String, required: true, trim: true },
-    phone: { type: Number, required: true, trim: true },
+    username: { type: String, },
+    email: { type: String, },
+    phone: { type: Number, },
     password: { type: String },
 });
 module.exports = mongoose.model('admin', admin);
